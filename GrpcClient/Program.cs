b4e2f709d9997341b4eb577d16e8003da107c926
danns88 <daniel_81_my@yahoo.com>
2022-08-18 18:49:37 +0800

    grpc net 6

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+
+//var input = new HelloRequest { Name = "wahlao eh" };
+//var channel = GrpcChannel.ForAddress("https://localhost:7202/");
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(input);
+//Console.WriteLine(reply.Message);
+
+var channel = GrpcChannel.ForAddress("https://localhost:7202/");
+var client = new Person.PersonClient(channel);
+
+var personRequest = new PersonLookupModel { UserId = 11 };
+
+var person = await client.GetPersonInfoAsync(personRequest);
+Console.WriteLine($"{person.FirstName}{Environment.NewLine}{person.LastName}{Environment.NewLine}{person.Age}{Environment.NewLine}{person.Email}");
+
+Console.WriteLine();
+Console.WriteLine("Get Newly Added Person with stream");
+
+using (var call = client.GetNewPerson(new NewPersonRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentPerson = call.ResponseStream.Current;
+        Console.WriteLine($"{currentPerson.FirstName}{Environment.NewLine}{currentPerson.LastName}{Environment.NewLine}{currentPerson.Age}{Environment.NewLine}{currentPerson.Email}");
+        Console.WriteLine();
+    }
+}
+
+Console.ReadLine();
---
+++ GrpcServer/Services/PersonService.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class PersonService : Person.PersonBase
+    {
+        private readonly ILogger<PersonService> _logger;
+
+        public PersonService(ILogger<PersonService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PersonModel> GetPersonInfo(PersonLookupModel request, ServerCallContext context)
+        {
+            PersonModel model = new PersonModel()
+            {
+                FirstName = $"Name {request.UserId}",
+                Age = request.UserId,
+                Email = $"Email {request.UserId}",
+                LastName = $"LastName {request.UserId}",
+            };
+
+            return Task.FromResult(model);
+        }
+
+        public override async Task GetNewPerson(NewPersonRequest request, IServerStreamWriter<PersonModel> responseStream, ServerCallContext context)
+        {
+            List<PersonModel> persons = new List<PersonModel>
+            {
+                new PersonModel()
+                {
+                    FirstName = "danns1",
+                    Age = 1,
+                    Email = "danns1@email.com",
+                    LastName = "danns1",
+                },
+                new PersonModel()
+                {
+                    FirstName = "danns2",
+                    Age = 21,
+                    Email = "danns2@email.com",
+                    LastName = "danns2",
+                },
+                new PersonModel()
+                {
+                    FirstName = "danns3",
+                    Age = 31,
+                    Email = "danns3@email.com",
+                    LastName = "danns3",
+                }
+            };
+            
+            foreach(var person in persons)
+            {
+                await Task.Delay(2000);
+                await responseStream.WriteAsync(person);
+            }
+        }
+    }
+}
